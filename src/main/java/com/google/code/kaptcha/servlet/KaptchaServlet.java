adfbf0933f982852a4c7f6924806da898dbbf799
ZhReimu <1725338233@qq.com>
2022-11-21 11:15:39 +0800

    改为使用 maven 构建 迁移 javax -> jakarta 格式化代码, 优化导入

--- src/main/java/com/google/code/kaptcha/servlet/KaptchaServlet.java
+++ src/main/java/com/google/code/kaptcha/servlet/KaptchaServlet.java
@@ -1,22 +1,21 @@
 package com.google.code.kaptcha.servlet;
 
+import com.google.code.kaptcha.Producer;
+import com.google.code.kaptcha.util.Config;
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.Date;
 import java.util.Enumeration;
 import java.util.Properties;
-
-import javax.imageio.ImageIO;
-import javax.servlet.Servlet;
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import com.google.code.kaptcha.Producer;
-import com.google.code.kaptcha.util.Config;
 
 /**
  * This servlet uses the settings passed into it via the Producer api.
@@ -25,78 +24,76 @@ import com.google.code.kaptcha.util.Config;
  * @author jon
  * @author cliffano
  */
-@SuppressWarnings("serial")
-public class KaptchaServlet extends HttpServlet implements Servlet
-{
-	private Properties props = new Properties();
+public class KaptchaServlet extends HttpServlet implements Servlet {
 
-	private Producer kaptchaProducer = null;
+    private final Properties props = new Properties();
 
-	private String sessionKeyValue = null;
+    private Producer kaptchaProducer = null;
 
-	private String sessionKeyDateValue = null;
+    private String sessionKeyValue = null;
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see javax.servlet.Servlet#init(javax.servlet.ServletConfig)
-	 */
-	@Override
-	public void init(ServletConfig conf) throws ServletException
-	{
-		super.init(conf);
+    private String sessionKeyDateValue = null;
 
-		// Switch off disk based caching.
-		ImageIO.setUseCache(false);
+    /*
+     * (non-Javadoc)
+     *
+     * @see javax.servlet.Servlet#init(javax.servlet.ServletConfig)
+     */
+    @Override
+    public void init(ServletConfig conf) throws ServletException {
+        super.init(conf);
 
-		Enumeration<?> initParams = conf.getInitParameterNames();
-		while (initParams.hasMoreElements())
-		{
-			String key = (String) initParams.nextElement();
-			String value = conf.getInitParameter(key);
-			this.props.put(key, value);
-		}
+        // Switch off disk based caching.
+        ImageIO.setUseCache(false);
 
-		Config config = new Config(this.props);
-		this.kaptchaProducer = config.getProducerImpl();
-		this.sessionKeyValue = config.getSessionKey();
-		this.sessionKeyDateValue = config.getSessionDate();
-	}
+        Enumeration<?> initParams = conf.getInitParameterNames();
+        while (initParams.hasMoreElements()) {
+            String key = (String) initParams.nextElement();
+            String value = conf.getInitParameter(key);
+            this.props.put(key, value);
+        }
 
-	/** */
-	@Override
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException
-	{
-		// Set to expire far in the past.
-		resp.setDateHeader("Expires", 0);
-		// Set standard HTTP/1.1 no-cache headers.
-		resp.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
-		// Set IE extended HTTP/1.1 no-cache headers (use addHeader).
-		resp.addHeader("Cache-Control", "post-check=0, pre-check=0");
-		// Set standard HTTP/1.0 no-cache header.
-		resp.setHeader("Pragma", "no-cache");
+        Config config = new Config(this.props);
+        this.kaptchaProducer = config.getProducerImpl();
+        this.sessionKeyValue = config.getSessionKey();
+        this.sessionKeyDateValue = config.getSessionDate();
+    }
 
-		// return a jpeg
-		resp.setContentType("image/jpeg");
+    /**
+     *
+     */
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        // Set to expire far in the past.
+        resp.setDateHeader("Expires", 0);
+        // Set standard HTTP/1.1 no-cache headers.
+        resp.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
+        // Set IE extended HTTP/1.1 no-cache headers (use addHeader).
+        resp.addHeader("Cache-Control", "post-check=0, pre-check=0");
+        // Set standard HTTP/1.0 no-cache header.
+        resp.setHeader("Pragma", "no-cache");
 
-		// create the text for the image
-		String capText = this.kaptchaProducer.createText();
+        // return a jpeg
+        resp.setContentType("image/jpeg");
 
-		// store the text in the session
-		req.getSession().setAttribute(this.sessionKeyValue, capText);
+        // create the text for the image
+        String capText = this.kaptchaProducer.createText();
 
-		// store the date in the session so that it can be compared
-		// against to make sure someone hasn't taken too long to enter
-		// their kaptcha
-		req.getSession().setAttribute(this.sessionKeyDateValue, new Date());
+        // store the text in the session
+        req.getSession().setAttribute(this.sessionKeyValue, capText);
 
-		// create the image with the text
-		BufferedImage bi = this.kaptchaProducer.createImage(capText);
+        // store the date in the session so that it can be compared
+        // against to make sure someone hasn't taken too long to enter
+        // their kaptcha
+        req.getSession().setAttribute(this.sessionKeyDateValue, new Date());
 
-		ServletOutputStream out = resp.getOutputStream();
+        // create the image with the text
+        BufferedImage bi = this.kaptchaProducer.createImage(capText);
 
-		// write the data out
-		ImageIO.write(bi, "jpg", out);
-	}
+        ServletOutputStream out = resp.getOutputStream();
+
+        // write the data out
+        ImageIO.write(bi, "jpg", out);
+    }
 }
